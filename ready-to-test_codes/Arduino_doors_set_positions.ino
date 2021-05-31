08dce4c34a769974803fdf4b8c6ad832a3811849
André Oliveira Borba <79326137+b-andreoliveira@users.noreply.github.com>
2021-05-31 13:38:39 +0200

    Update Arduino_doors_set_positions.ino

--- ready-to-test_codes/Arduino_doors_set_positions.ino
+++ ready-to-test_codes/Arduino_doors_set_positions.ino
@@ -4,33 +4,33 @@ Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
-int neutral = 100;
-int food = 120;
-int social = 75;
+int neutral = 70;
+int food = 100;
+int social = 95;
 
 // SERVO 1
+// neutral = 100
 // food = 75
 // social = 120
-// closed = 100
 
 // SERVO 2
+// neutral = 80
 // food = 103
 // social = 56
-// closed = 80
 
 // SERVO 3
+// neutral = 80
+// food = 100
+// social = 60
+
+// SERVO 4
+// neutral = 70 
 // food = 45
 // social = 95
-// closed = 75
-
-// SERVO 4 
-// food = 110
-// social = 50
-// closed = 75
 
 
 void setup() {
-  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
